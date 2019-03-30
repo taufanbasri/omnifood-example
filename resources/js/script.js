abc191f8484ac606647bedc08f6cb58e09d6884e
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2019-03-31 00:01:08 +0700

    Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-        offset: '60px;'
+        offset: '60px'
     });
     
     /* Scroll on button */
@@ -55,5 +55,30 @@ $(document).ready(function () {
         });
         }
     }
+    });
+
+    /** Animations on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
     });
 });
