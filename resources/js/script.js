26d261cb6ed161f91efb9bdf11526b25521cd0d3
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2019-03-31 19:59:53 +0700

    Making the navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -81,4 +81,20 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     });
+
+    /** Mobile nav */
+    $('.js--nav-icon').click(function () { 
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-md-menu')) {
+            icon.addClass('ion-md-close');
+            icon.removeClass('ion-md-menu');
+        } else {
+            icon.addClass('ion-md-menu');
+            icon.removeClass('ion-md-close');
+        }
+    });
 });
